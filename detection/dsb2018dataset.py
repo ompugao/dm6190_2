eed2d4c19729d741acb5866408e878e5b861edff
Shohei Fujii <fujii.shohei@gmail.com>
2020-10-27 14:04:09 +0900

    mask needed to be normalized

--- detection/dsb2018dataset.py
+++ detection/dsb2018dataset.py
@@ -27,7 +27,11 @@ class DSB2018TrainDataset(VisionDataset):
         for maskpath in (parentpath / 'masks').iterdir():
             maskid = maskpath.name[:-len(maskpath.suffix)]
             #masks.append((maskid, np.array(Image.open(maskpath))))
-            masks.append(np.array(Image.open(maskpath)))
+            mask = np.array(Image.open(maskpath))
+            # normalize
+            mask = (mask - mask.min()) / (mask.max() - mask.min())
+            masks.append(mask)
+
         masks = np.array(masks)
 
         # get bounding box coordinates for each mask
@@ -66,8 +70,7 @@ class DSB2018TrainDataset(VisionDataset):
             img = img.float()
             masks = aug['masks']
             boxes = aug['bboxes']
-
-            area = compute_area(boxes)
+            area = compute_area(np.array(boxes))
 
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
         masks = torch.as_tensor(masks, dtype=torch.uint8)
@@ -107,20 +110,73 @@ class DSB2018TestDataset(VisionDataset):
     def __len__(self):
         return len(self.datapaths)
 
+# run length encoding/decoding
+
+def rle_encode(mask):
+    assert mask.ndim == 2, "Mask must be of shape [Height, Width]"
+    # Flatten it column wise
+    m = mask.T.flatten()
+    # Compute gradient. Equals 1 or -1 at transition points
+    g = np.diff(np.concatenate([[0], m, [0]]), n=1)
+    # 1-based indicies of transition points (where gradient != 0)
+    rle = np.where(g != 0)[0].reshape([-1, 2]) + 1
+    # Convert second index in each pair to lenth
+    rle[:, 1] = rle[:, 1] - rle[:, 0]
+    return " ".join(map(str, rle.flatten()))
+
+
+def rle_decode(rle, shape):
+    rle = list(map(int, rle.split()))
+    rle = np.array(rle, dtype=np.int32).reshape([-1, 2])
+    rle[:, 1] += rle[:, 0]
+    rle -= 1
+    mask = np.zeros([shape[0] * shape[1]], np.bool)
+    for s, e in rle:
+        assert 0 <= s < mask.shape[0]
+        assert 1 <= e <= mask.shape[0], "shape: {}  s {}  e {}".format(shape, s, e)
+        mask[s:e] = 1
+    # Reshape and transpose
+    mask = mask.reshape([shape[1], shape[0]]).T
+    return mask
+
+
+def mask_to_rle(image_id, mask, scores):
+    "Encodes instance masks to submission format."
+    assert mask.ndim == 3, "Mask must be [H, W, count]"
+    # If mask is empty, return line with image ID only
+    if mask.shape[-1] == 0:
+        return "{},".format(image_id)
+    # Remove mask overlaps
+    # Multiply each instance mask by its score order
+    # then take the maximum across the last dimension
+    order = np.argsort(scores)[::-1] + 1  # 1-based descending
+    mask = np.max(mask * np.reshape(order, [1, 1, -1]), -1)
+    # Loop over instance masks
+    lines = []
+    for o in order:
+        m = np.where(mask == o, 1, 0)
+        # Skip if empty
+        if m.sum() == 0.0:
+            continue
+        rle = rle_encode(m)
+        lines.append("{}, {}".format(image_id, rle))
+    return "\n".join(lines)
+
 if __name__ == '__main__':
     import albumentations
     from albumentations.pytorch.transforms import ToTensorV2
     data_transforms = albumentations.Compose([
-        #albumentations.Flip(),
-        #albumentations.RandomBrightness(0.2),
-        #albumentations.ShiftScaleRotate(rotate_limit=90, scale_limit=0.10),
+        albumentations.Flip(),
+        albumentations.RandomBrightness(0.2),
+        albumentations.ShiftScaleRotate(rotate_limit=90, scale_limit=0.10),
         #albumentations.Normalize(),
-        #albumentations.Resize(256, 256),
+        albumentations.Resize(512, 512),
         ToTensorV2()
         ], bbox_params=albumentations.BboxParams(format='pascal_voc', label_fields=['class_labels']))
 
     traindataset = DSB2018TrainDataset('../data-science-bowl-2018/stage1_train/', transforms=data_transforms)
     testdataset = DSB2018TestDataset('../data-science-bowl-2018/stage1_test/', transform = data_transforms)
+    self = traindataset
 
     from IPython.terminal import embed; ipshell=embed.InteractiveShellEmbed(config=embed.load_default_config())(local_ns=locals())
 
